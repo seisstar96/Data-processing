d1fb37665138add4092e3710c0814c6705ae140d
Weifan LU <luweifan001@qq.com>
2019-03-12 12:46:16 +0800

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,1 +1,10 @@
-transfer from polezero subtype plo1 to none freq 0.001 0.002 10 20
+for cont in `gawk '{print $1}' cont.list`
+do
+	cd $cont
+	for wf in `ls *SAC`
+	do
+       echo $wf | gawk '{print"r "$1"\ntransfer from polezero subtype plo1 to none freq 0.001 0.002 10 20\nw append .bp\nquit"}' | sac
+  done
+  cd ..
+done
+       
