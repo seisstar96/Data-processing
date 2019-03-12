3f34a2d667ae2dfe6f1af9f4a72e53587c62592a
Weifan LU <luweifan001@qq.com>
2019-03-12 12:52:58 +0800

    Update rmIR.sh

--- rmIR.sh
+++ rmIR.sh
@@ -3,7 +3,7 @@ do
 	cd $cont
 	for wf in `ls *SAC`
 	do
-       echo $wf | gawk '{print"r "$1"\ntransfer from polezero subtype plo1 to none freq 0.001 0.002 10 20\nw append .bp\nquit"}' | sac
+        echo $wf | gawk '{print"r "$1"\ntransfer from polezero subtype plo1 to none freq 0.001 0.002 10 20\nw append .bp\nquit"}' | sac
   done
   cd ..
 done
